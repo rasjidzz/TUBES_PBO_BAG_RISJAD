6d147cc7d29b668201e8c0d9528c1240d2833179
Muhammad Risjad Shidqi Febian <71804153+rasjidzz@users.noreply.github.com>
2023-05-16 14:22:02 +0700

    Add files via upload
    Kecuali Node_modules

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["Dashboard.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#06b6d4",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
